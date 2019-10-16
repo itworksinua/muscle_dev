7d61fa0a9bb68a10462f781794300b9532cf6885
Dmitriy Davidov <email@mail.com>
2019-10-17 01:06:40 +0300

    Init

---
+++ MuscleGun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MuscleGun
+//
+//  Created by Admin on 10/17/19.
+//  Copyright © 2019 ItWorksinUA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
